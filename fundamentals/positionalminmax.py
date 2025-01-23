94bd839c5abf835e10a87917020d4f20a015ff0f
ahmsyr <amados1978@gmail.com>
2025-01-23 13:09:44 +0300

    delete folder for errors

--- fundamentals/positionalminmax.py
+++
@@ -1,7 +0,0 @@
-import torch
-tensor = torch.arange(10, 100, 10)
-
-print(f"Tensor: {tensor}")
-
-print(f"Index of max value: {tensor.argmax()}")
-print(f"Index of min value: {tensor.argmin()}")
--- fundamentals/random_tensors.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-random_tensor =  torch.rand(size=(3,4))
-print(random_tensor)
-print(random_tensor.dtype)
-
-random_image_size_tensor = torch.rand(size=(224,224,3))
-print(random_image_size_tensor.shape, random_image_size_tensor.ndim)
